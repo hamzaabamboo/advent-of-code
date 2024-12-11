e4aa7b37acf4ffae39acfe42021262bb2ba8420a
Tanyawat Vittaypalotai <vittayapalotai.tanyawat@arsaga.jp>
2024-12-11 20:09:02 +0900

    feat: day 10

---
+++ 2024/10/main.py
@@ -0,0 +1,40 @@
+grid = open("./input.txt").read().splitlines()
+# grid = open("./test.txt").read().splitlines()
+
+starting_points = []
+for j in range(len(grid)):
+    for i in range(len(grid[j])):
+        if grid[j][i] == "0":
+            starting_points.append((i, j))
+
+# q1
+total_points = 0
+for i, j in starting_points:
+    current_point = 0
+    q = [((i, j), 1)]
+    visited = []
+    while len(q) > 0:
+        (x, y), n = q.pop(0)
+        # print(q)
+        # if (x, y) in visited:
+        #     continue
+        # visited.append((x, y))
+        if n == 10 and int(grid[y][x]) == 9:
+            print("POINT!", x, y)
+            current_point += 1
+            continue
+        if y + 1 < len(grid) and int(grid[y + 1][x]) == n:
+            q.append(((x, y + 1), n + 1))
+        if y - 1 >= 0 and int(grid[y - 1][x]) == n:
+            q.append(((x, y - 1), n + 1))
+        if x + 1 < len(grid[0]) and int(grid[y][x + 1]) == n:
+            q.append(((x + 1, y), n + 1))
+        if x - 1 >= 0 and int(grid[y][x - 1]) == n:
+            q.append(((x - 1, y), n + 1))
+
+    total_points += current_point
+    print(current_point)
+    print("---")
+
+
+print(total_points)
